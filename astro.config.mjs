0b7191b3461025df6ecefc19702bde15de0fcfc4
Berke Akçen <berkeakcen@gmail.com>
2025-11-20 14:41:33 +0300

    Upload project files

---
+++ astro.config.mjs
@@ -0,0 +1,54 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Plugin Docs',
+			social: [
+                { icon: 'github', label: 'GitHub', href: 'https://github.com/Despical' },
+                { icon: 'discord', label: 'GitHub', href: 'https://discord.gg/uXVU8jmtpU' }
+            ],
+            lastUpdated: true,
+            editLink: {
+                baseUrl: "https://github.com/Despical/docs/edit/main/",
+            },
+            logo: {
+                src: "/src/assets/despical-light.png"
+            },
+            sidebar: [
+                {
+                    label: 'Easter Eggs',
+                    items: [
+                        {
+                            label: 'Getting Started',
+                            items: [
+                                {label: 'Installation', slug: 'easter-eggs/get-started'},
+                                {label: 'Creating new eggs', slug: 'easter-eggs/creating-new-eggs'},
+                            ],
+                        },
+                        {
+                            label: 'Placeholders',
+                            items: [
+                                {label: 'PlaceholderAPI', slug: 'easter-eggs/placeholders'}
+                            ],
+                        },
+                    ],
+                },
+            ],
+            customCss: [
+                "@fontsource/poppins/400.css",
+                "@fontsource/jetbrains-mono/400.css",
+                "@fontsource/jetbrains-mono/600.css",
+                "./src/styles/custom.css",
+            ],
+            components: {
+                SiteTitle: "./src/components/overrides/SiteTitle.astro",
+                Footer: "./src/components/overrides/Footer.astro",
+                LastUpdated: "./src/components/overrides/LastUpdated.astro",
+            }
+		}),
+	],
+});
