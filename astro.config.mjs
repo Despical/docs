fa5a59a0b8427ab0491abe3e06b28eebedf05f31
Berke Akçen <berkeakcen@gmail.com>
2025-11-20 15:04:24 +0300

    Update Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://despical.github.io',
+    base: '/docs',
 	integrations: [
 		starlight({
 			title: 'Plugin Docs',
